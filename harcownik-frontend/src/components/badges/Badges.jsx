c9bd1fd2b21ae63ea9356e0ed452e07dbdf503d1
Michal170 <michalwlosek170@gmail.com>
2023-03-30 19:13:57 +0200

    accordion

--- harcownik-frontend/src/components/badges/Badges.jsx
+++ harcownik-frontend/src/components/badges/Badges.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import { Accordion, Card, Button } from 'react-bootstrap';
 import BadgeItem from './BadgeItem';
 
+
+// const Badges = () => {
+//   return (
+//     <div>
+//       <div className="jumbotron">
+//         <h1 className="display-4 text-center">Harcownik</h1>
+        
+        
+//       </div>
+//     </div>);
+// }
+
 const Badges = () => {
   const samerytanskieBadges = [
     {
@@ -29,39 +41,67 @@ const Badges = () => {
       image: 'https://via.placeholder.com/150',
     },
     // add more badges here
-  ];
+  ]
 
   return (
     <Accordion defaultActiveKey="0">
-      <Card>
-        <Card.Header>
-          <Accordion.Toggle as={Button} variant="link" eventKey="0">
-            Sprawności Samerytańskie
-          </Accordion.Toggle>
-        </Card.Header>
-        <Accordion.Collapse eventKey="0">
-          <Card.Body>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Sprawności Samerytańskie</Accordion.Header>
+        <Accordion.Body>
             {samerytanskieBadges.map((badge, index) => (
               <BadgeItem key={index} badge={badge} />
             ))}
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-      <Card>
-        <Card.Header>
-          <Accordion.Toggle as={Button} variant="link" eventKey="1">
-            Sprawnościowe
-          </Accordion.Toggle>
-        </Card.Header>
-        <Accordion.Collapse eventKey="1">
-          <Card.Body>
+          
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Sprawności Sprawnościowe</Accordion.Header>
+        <Accordion.Body>
             {sprawnoscioweBadges.map((badge, index) => (
               <BadgeItem key={index} badge={badge} />
             ))}
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
+          
+        </Accordion.Body>
+      </Accordion.Item>
     </Accordion>
+
+
+
+
+
+
+
+// Accordion defaultActiveKey="0"
+//     <Accordion defaultActiveKey="0">
+//       <Card>
+//         <Card.Header>
+//           <Accordion.Toggle as={Button} variant="link" eventKey="0">
+//             Sprawności Samerytańskie
+//           </Accordion.Toggle>
+//         </Card.Header>
+//         <Accordion.Collapse eventKey="0">
+//           <Card.Body>
+//             {samerytanskieBadges.map((badge, index) => (
+//               <BadgeItem key={index} badge={badge} />
+//             ))}
+//           </Card.Body>
+//         </Accordion.Collapse>
+//       </Card>
+//       <Card>
+//         <Card.Header>
+//           <Accordion.Toggle as={Button} variant="link" eventKey="1">
+//             Sprawnościowe
+//           </Accordion.Toggle>
+//         </Card.Header>
+//         <Accordion.Collapse eventKey="1">
+//           <Card.Body>
+//             {sprawnoscioweBadges.map((badge, index) => (
+//               <BadgeItem key={index} badge={badge} />
+//             ))}
+//           </Card.Body>
+//         </Accordion.Collapse>
+//       </Card>
+//     </Accordion>
   );
 };
 
