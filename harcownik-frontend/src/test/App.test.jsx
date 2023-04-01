e2188ce955192d795dc299bf1bc454b44e483abf
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-04-01 16:39:35 +0200

    add basic test

--- harcownik-frontend/src/test/App.test.jsx
+++ harcownik-frontend/src/test/App.test.jsx
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import App from '../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Main app show corectly', () => {
+  render(<App />)
+  const textElemnt = screen.getByText('Witaj w aplikacji Harcownik')
+  expect(textElemnt).toBeInTheDocument()
+  
+})
+
+test('Raport is avaible', () => {
+  render(<App />)
+  const textElemnt = screen.getByText('Czym jest Harcownik?')
+  expect(textElemnt).toBeInTheDocument()
+  
+})
