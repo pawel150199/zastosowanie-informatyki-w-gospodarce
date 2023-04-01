fbc28dc3dd7aa5ce5e2f4f88c2c38221cc8687ba
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-04-01 16:41:01 +0200

    add basic test

--- harcownik-frontend/src/test/App.test.jsx
+++ harcownik-frontend/src/test/App.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import App from '../App';
+import About from '../components/about/About';
 
 test('Main app show corectly', () => {
   render(<App />)
@@ -14,4 +15,10 @@ test('Raport is avaible', () => {
   const textElemnt = screen.getByText('Czym jest Harcownik?')
   expect(textElemnt).toBeInTheDocument()
   
+})
+
+test('About page renders correctly', () => {
+  render(<About />)
+  const textElement = screen.getByText('Harcownik to aplikacja webowa skierowana do organizacji harcerskich. Ma ona za zadanie usprawnić zarządzanie harcerzykami.')
+  expect(textElement).toBeInTheDocument()
 })
