491de4246240b833bfd2877c4791e6b06120aaf1
pawel150199 <pawel.polski99@gmail.com>
2023-04-01 20:38:15 +0200

    modify badges page

--- harcownik-frontend/src/components/badges/BadgeItem.jsx
+++ harcownik-frontend/src/components/badges/BadgeItem.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 const BadgeItem = ({ badge }) => {
   return (
     <Card>
-      <Card.Img variant="top" src={badge.image} />
+      <div style={{ position: 'relative' }}>
+        <Card.Img variant="top" src={badge.image} />
+        <div style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          padding: '0.5rem',
+        }}>
+          <Card.Title style={{ color: 'white' }}>{badge.name}</Card.Title>
+        </div>
+      </div>
       <Card.Body>
-        <Card.Title>{badge.name}</Card.Title>
-        <Card.Text>{badge.description}</Card.Text>
-        <Button variant="primary">Learn More</Button>
+        {badge.description.map((text, index) => (
+          <Card.Text key={index} style={{ margin: '0.5rem 0' }}> {text} </Card.Text>
+        ))}
       </Card.Body>
     </Card>
   );
