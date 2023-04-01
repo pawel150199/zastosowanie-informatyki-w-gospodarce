ed1ed493400e528bd70103e98081d2b09c78120c
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-04-01 17:20:35 +0200

    test

--- harcownik-frontend/src/test/App.test.jsx
+++ harcownik-frontend/src/test/App.test.jsx
@@ -2,23 +2,19 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import App from '../App';
 import About from '../components/about/About';
+import UserPage from '../components/user/User';
 
 test('Main app show corectly', () => {
   render(<App />)
-  const textElemnt = screen.getByText('Witaj w aplikacji Harcownik')
-  expect(textElemnt).toBeInTheDocument()
-  
-})
-
-test('Raport is avaible', () => {
-  render(<App />)
-  const textElemnt = screen.getByText('Czym jest Harcownik?')
-  expect(textElemnt).toBeInTheDocument()
+  expect(screen.getByText('Witaj w aplikacji Harcownik')).toBeInTheDocument()
+  expect(screen.getByText('Czym jest Harcownik?')).toBeInTheDocument()
+  expect(screen.getByText('Features')).toBeInTheDocument()
   
 })
 
 test('About page renders correctly', () => {
   render(<About />)
-  const textElement = screen.getByText('Harcownik to aplikacja webowa skierowana do organizacji harcerskich. Ma ona za zadanie usprawnić zarządzanie harcerzykami.')
-  expect(textElement).toBeInTheDocument()
+  expect(screen.getByText('Harcownik to aplikacja webowa skierowana do organizacji harcerskich. Ma ona za zadanie usprawnić zarządzanie harcerzykami.')).toBeInTheDocument()
+  expect(screen.getByText('Harcownik')).toBeInTheDocument()
+  expect(screen.getByText('Nasz zespół')).toBeInTheDocument()
 })
