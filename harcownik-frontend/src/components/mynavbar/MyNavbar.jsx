e68ff39b34b115f66680152b60eee177ea789814
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-04-06 19:07:19 +0200

    add tests and center MyNavBar

--- harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
+++ harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
@@ -5,19 +5,30 @@ import  './mynavbar.css'
 const MyNavbar = () => {
 
     return (
-        <Navbar  expand="lg" className="navbar-nav navbar-dark bg-dark h1 fs-4"> 
+        <Navbar bg="light" expand="lg" className="navbar-nav  h1 fs-5" > 
             <Container>
                 <Navbar.Brand href="/" className="navbar-nav mr-left">
                         <Image src="/img/logo.png" id="img" style={{width: "1.8em"}}/>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-left" />
-                <Navbar.Collapse id="basic-navbar-nav" >
-                    <Nav className="navbar-nav mx-auto" >
-                        <Nav.Link className="box" href="/" >Strona Główna</Nav.Link>
-                        <Nav.Link className="box" href="/login"> Logowanie</Nav.Link>
-                        <Nav.Link className="box" href="/about"> O nas</Nav.Link>
-                        <Nav.Link className="box" href="/badges"> Sprawności</Nav.Link>
-                        <Nav.Link className="box" href="/raport"> Raport</Nav.Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-left"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="navbar-nav mx-auto">
+=======
+        <Navbar bg="light" expand="lg" className="justify-content-center">
+            <Container>
+            <Nav.Link href="/">
+                {/* <Image src="/img/logo.jpeg"  style={{width: "3em", marginRight: "4em"}}/> */}
+            </Nav.Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-left"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="navbar-nav mx-auto">
+                        <Image src="/img/logo.jpeg"  style={{width: "3em", marginRight: "3em"}}/>
+>>>>>>> 7109308 (add tests and center MyNavBar)
+                        <Nav.Link href="/">Strona Główna</Nav.Link>
+                        <Nav.Link href="/login">Logowanie</Nav.Link>
+                        <Nav.Link href="/about">O nas</Nav.Link>
+                        <Nav.Link href="/badges">Sprawności</Nav.Link>
+                        <Nav.Link href="/raport">Raport</Nav.Link>
                     </Nav>
                     <Nav className="navbar-nav navbar-dark mr-auto">
                         <NavDropdown title="Menu" id="basic-nav-dropdown">
