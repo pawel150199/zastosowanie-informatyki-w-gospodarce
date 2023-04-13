80710c7ad1be2e16e08240056507d0ccf50f200a
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-04-13 18:48:42 +0200

    Navbar (#9)
    * add tests and center MyNavBar
    
    * add tests and center MyNavBar
    
    * Center MyNavBar + add DropDownMenu

--- harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
+++ harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
@@ -1,22 +1,27 @@
-import { Navbar, Nav, Container, Image} from 'react-bootstrap';
+import { Navbar, Nav, Container, Image, NavDropdown} from 'react-bootstrap';
 import React from 'react'
 
 const MyNavbar = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="lg" className="navbar-nav  h1 fs-5" > 
             <Container>
-            <Nav.Link href="/">
-                <Image src="/img/logo.jpeg"  style={{width: "3em", marginRight: "4em"}}/>
-            </Nav.Link>
+                <Navbar.Brand href="/" className="navbar-nav mr-left">
+                        <Image src="/img/logo.jpeg"  style={{width: "3em"}}/>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-left"/>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-left">
+                    <Nav className="navbar-nav mx-auto">
                         <Nav.Link href="/">Strona Główna</Nav.Link>
                         <Nav.Link href="/login">Logowanie</Nav.Link>
                         <Nav.Link href="/about">O nas</Nav.Link>
                         <Nav.Link href="/badges">Sprawności</Nav.Link>
-                        <Nav.Link href="/user">Użytkownik</Nav.Link>
                         <Nav.Link href="/raport">Raport</Nav.Link>
+                    </Nav>
+                    <Nav className="navbar-nav mr-auto">
+                        <NavDropdown title="Menu" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/user">Profil</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Wyloguj</NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
@@ -25,3 +30,4 @@ const MyNavbar = () => {
 }
 
 export default MyNavbar;
+
