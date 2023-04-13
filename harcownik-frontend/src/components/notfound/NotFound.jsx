60d20effe83ec867a3ae0464272f8c6c9e8444da
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-04-13 19:21:13 +0200

    Add nice 404 page

--- harcownik-frontend/src/components/notfound/NotFound.jsx
+++ harcownik-frontend/src/components/notfound/NotFound.jsx
@@ -6,7 +6,6 @@ const NotFound = () => {
                   <meta charSet="UTF-8" />
                   <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                  <title>Bootstrap 5 404 Error Page</title>
                   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
                   <div className="d-flex align-items-center justify-content-center vh-100">
                     <div className="text-center">
