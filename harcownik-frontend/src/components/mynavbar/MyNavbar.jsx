5fdec6c97d8c5b8e7086b4eaaf15ff57839c974d
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-04-15 20:31:28 +0200

    navbar hover

--- harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
+++ harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
@@ -1,37 +1,31 @@
 import { Navbar, Nav, Container, Image, NavDropdown} from 'react-bootstrap';
-import React from 'react'
-import  './mynavbar.css'
+// import {React, useState} from 'react'
+import {React} from 'react'
+// import styles from './style.css'
+import  './style.css'
+
+
 
 const MyNavbar = () => {
 
+
     return (
-        <Navbar bg="light" expand="lg" className="navbar-nav  h1 fs-5" > 
+         <Navbar className="navbar-nav navbar-dark bg-dark h1 fs-4">  
             <Container>
                 <Navbar.Brand href="/" className="navbar-nav mr-left">
                         <Image src="/img/logo.png" id="img" style={{width: "1.8em"}}/>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-left"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="navbar-nav mx-auto">
-=======
-        <Navbar bg="light" expand="lg" className="justify-content-center">
-            <Container>
-                <Navbar.Brand href="/" className="navbar-nav mr-left">
-                        <Image src="/img/logo.jpeg"  style={{width: "3em"}}/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-left"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="navbar-nav mx-auto">
-                        <Image src="/img/logo.jpeg"  style={{width: "3em", marginRight: "3em"}}/>
->>>>>>> 7109308 (add tests and center MyNavBar)
-                        <Nav.Link href="/">Strona Główna</Nav.Link>
-                        <Nav.Link href="/login">Logowanie</Nav.Link>
-                        <Nav.Link href="/about">O nas</Nav.Link>
-                        <Nav.Link href="/badges">Sprawności</Nav.Link>
-                        <Nav.Link href="/raport">Raport</Nav.Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-left" />
+                <Navbar.Collapse id="basic-navbar-nav" >
+                    <Nav className="navbar-nav mx-auto" >
+                        <Nav.Link className="box" href="/" >Strona Główna</Nav.Link>
+                        <Nav.Link className="box" href="/login"> Logowanie</Nav.Link>
+                        <Nav.Link className="box" href="/about"> O nas</Nav.Link>
+                        <Nav.Link className="box" href="/badges"> Sprawności</Nav.Link>
+                        <Nav.Link className="box" href="/raport"> Raport</Nav.Link>
                     </Nav>
-                    <Nav className="navbar-nav navbar-dark mr-auto">
-                        <NavDropdown title="Menu" id="basic-nav-dropdown">
+                    <Nav className="navbar-nav mr-auto">
+                        <NavDropdown className="box" title="Menu" id="basic-nav-dropdown">
                             <NavDropdown.Item href="/user">Profil</NavDropdown.Item>
                             <NavDropdown.Item href="#">Wyloguj</NavDropdown.Item>
                         </NavDropdown>
