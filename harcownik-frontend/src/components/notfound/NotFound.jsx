b02ca639337b4dfaaec6ae122a629487705171c5
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-04-15 09:15:01 +0200

    fix commit

--- harcownik-frontend/src/components/notfound/NotFound.jsx
+++ harcownik-frontend/src/components/notfound/NotFound.jsx
@@ -2,22 +2,17 @@ import React from 'react'
 
 const NotFound = () => {
     return (
-                <div>
-                  <meta charSet="UTF-8" />
-                  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
-                  <div className="d-flex align-items-center justify-content-center vh-100">
+                <div className="d-flex align-items-center justify-content-center vh-100">
                     <div className="text-center">
                       <h1 className="display-1 fw-bold">404</h1>
-                      <p className="fs-2"> <span className="text-danger">Opps!</span> Strona nie została znaleziona.</p>
+                      <p className="fs-2"> <span className="text-danger">Upps!</span> Strona nie została znaleziona.</p>
                       <p className="lead">
                         Strona której szukasz nie istnieje.
                       </p>
                       <a href="/" className="btn btn-primary">Wróć do strony głównej</a>
                     </div>
-                  </div>
                 </div>
+                
               
     );
 }
