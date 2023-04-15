4803011f613cb79b73fa7f0f38e69ac48e408ef1
Kasia Duszyńska <kasiaduszynska@MacBook-Air-Kasia.local>
2023-04-15 16:01:09 +0200

    change footer style

--- harcownik-frontend/src/components/footer/Footer.jsx
+++ harcownik-frontend/src/components/footer/Footer.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-
+import './footer.css'
 function Footer() {
   const year = new Date().getFullYear();
 
   return (
     <footer className="bg-light">
       <Container>
-        <p className="text-center">
-          &copy; {year} Harcownik. All rights reserved.
-        </p>
-        <p className="text-center">
+        <p className="text-center" id="style-1">
           Today's date is: {new Date().toLocaleDateString()}
+        </p>
+        <p className="text-center" id="style-2">
+          &copy; {year} Harcownik. All rights reserved.
         </p>
       </Container>
     </footer>
