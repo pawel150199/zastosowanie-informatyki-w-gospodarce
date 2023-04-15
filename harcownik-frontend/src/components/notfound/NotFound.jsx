325f3f53e0597f3da175dd5063760511e8dd6a8c
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-04-15 14:43:36 +0200

    New not found page (#11)
    * add tests and center MyNavBar
    
    * add tests and center MyNavBar
    
    * Center MyNavBar + add DropDownMenu
    
    * Add nice 404 page
    
    * Add nice 404 page
    
    * fix commit

--- harcownik-frontend/src/components/notfound/NotFound.jsx
+++ harcownik-frontend/src/components/notfound/NotFound.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>
-                Not Found
-            </h1>
-        </div>
+                <div className="d-flex align-items-center justify-content-center vh-100">
+                    <div className="text-center">
+                      <h1 className="display-1 fw-bold">404</h1>
+                      <p className="fs-2"> <span className="text-danger">Upps!</span> Strona nie została znaleziona.</p>
+                      <p className="lead">
+                        Strona której szukasz nie istnieje.
+                      </p>
+                      <a href="/" className="btn btn-primary">Wróć do strony głównej</a>
+                    </div>
+                </div>
+                
+              
     );
 }
 
