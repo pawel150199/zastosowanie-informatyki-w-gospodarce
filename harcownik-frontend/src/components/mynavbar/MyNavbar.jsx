dd4638040c8b4daaa5dd5dfb3371b3b94d4c9aa6
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-04-16 19:57:42 +0200

    Hover nav (#14)
    * add tests and center MyNavBar
    
    * add tests and center MyNavBar
    
    * Center MyNavBar + add DropDownMenu
    
    * Add nice 404 page
    
    * Add nice 404 page
    
    * fix commit
    
    * navbar hover
    
    * add style.css
    
    * hover NavBar
    
    * small change of navbar
    
    ---------
    
    Co-authored-by: pawel150199 <pawel.polski99@gmail.com>

--- harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
+++ harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
@@ -1,23 +1,25 @@
 import { Navbar, Nav, Container, Image, NavDropdown} from 'react-bootstrap';
 import React from 'react'
+import  './mynavbar.css'
 
 const MyNavbar = () => {
+
     return (
-        <Navbar bg="light" expand="lg" className="navbar-nav  h1 fs-5" > 
+        <Navbar  expand="lg" className="navbar-nav navbar-dark bg-dark h1 fs-4"> 
             <Container>
                 <Navbar.Brand href="/" className="navbar-nav mr-left">
-                        <Image src="/img/logo.jpeg"  style={{width: "3em"}}/>
+                        <Image src="/img/logo.png" id="img" style={{width: "1.8em"}}/>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-left"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="navbar-nav mx-auto">
-                        <Nav.Link href="/">Strona Główna</Nav.Link>
-                        <Nav.Link href="/login">Logowanie</Nav.Link>
-                        <Nav.Link href="/about">O nas</Nav.Link>
-                        <Nav.Link href="/badges">Sprawności</Nav.Link>
-                        <Nav.Link href="/raport">Raport</Nav.Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-left" />
+                <Navbar.Collapse id="basic-navbar-nav" >
+                    <Nav className="navbar-nav mx-auto" >
+                        <Nav.Link className="box" href="/" >Strona Główna</Nav.Link>
+                        <Nav.Link className="box" href="/login"> Logowanie</Nav.Link>
+                        <Nav.Link className="box" href="/about"> O nas</Nav.Link>
+                        <Nav.Link className="box" href="/badges"> Sprawności</Nav.Link>
+                        <Nav.Link className="box" href="/raport"> Raport</Nav.Link>
                     </Nav>
-                    <Nav className="navbar-nav mr-auto">
+                    <Nav className="navbar-nav navbar-dark mr-auto">
                         <NavDropdown title="Menu" id="basic-nav-dropdown">
                             <NavDropdown.Item href="/user">Profil</NavDropdown.Item>
                             <NavDropdown.Item href="#">Wyloguj</NavDropdown.Item>
