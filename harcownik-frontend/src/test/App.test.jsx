4140a2bb1118aaa5ae7902265c8270ecfeedb179
pawel150199 <pawel.polski99@gmail.com>
2023-04-17 16:29:18 +0200

    delete Raport from tests

--- harcownik-frontend/src/test/App.test.jsx
+++ harcownik-frontend/src/test/App.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'
+import React from 'react';
 import App from '../App';
 import About from '../components/about/About';
 import Badges from '../components/badges/Badges';
