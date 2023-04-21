78a0cd2b21ac0fb73fea46d71d5e068cdb5252ce
pawel150199 <pawel.polski99@gmail.com>
2023-04-21 22:42:38 +0200

    fix schema migration

--- harcownik-backend/alembic/env.py
+++ harcownik-backend/alembic/env.py
@@ -1,9 +1,12 @@
+
+from __future__ import with_statement
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+import os
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -16,7 +19,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from src.db.db import Base
+from src.db.base import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
