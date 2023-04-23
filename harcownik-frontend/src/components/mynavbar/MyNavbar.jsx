adb667169dd8dbea7bdcd623731fb50037bf2472
Paweł Polski <41188005+pawel150199@users.noreply.github.com>
2023-04-23 11:38:41 +0200

    Update MyNavbar.jsx

--- harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
+++ harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
@@ -4,10 +4,8 @@ import  "./mynavbar.css";
 
 const MyNavbar = () => {
 
-
     return (
-        <Navbar  expand="lg" className="navbar-nav navbar-dark bg-dark h1 fs-5" > 
-         {/* <Navbar className="navbar-nav navbar-dark bg-dark h1 fs-4">   */}
+        <Navbar  expand="lg" className="navbar-nav navbar-dark bg-dark h1 fs-4"> 
             <Container>
                 <Navbar.Brand href="/" className="navbar-nav mr-left">
                         <Image src="/img/logo.png" id="img" style={{width: "1.8em"}}/>
@@ -19,10 +17,9 @@ const MyNavbar = () => {
                         <Nav.Link className="box" href="/login"> Logowanie</Nav.Link>
                         <Nav.Link className="box" href="/about"> O nas</Nav.Link>
                         <Nav.Link className="box" href="/badges"> Sprawności</Nav.Link>
-                        <Nav.Link className="box" href="/raport"> Generuj rozkaz</Nav.Link>
                     </Nav>
-                    <Nav className="navbar-nav mr-auto">
-                        <NavDropdown className="box" title="Menu" id="basic-nav-dropdown">
+                    <Nav className="navbar-nav navbar-dark mr-auto">
+                        <NavDropdown title="Menu" id="basic-nav-dropdown">
                             <NavDropdown.Item href="/user">Profil</NavDropdown.Item>
                             <NavDropdown.Item href="#">Wyloguj</NavDropdown.Item>
                         </NavDropdown>
@@ -34,4 +31,3 @@ const MyNavbar = () => {
 };
 
 export default MyNavbar;
-
