ca973e5c2d11230996d4659f253fff9f1d40f870
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-04-28 22:34:08 +0200

    next button

--- harcownik-frontend/src/components/userRequests/UserEfficiency.jsx
+++ harcownik-frontend/src/components/userRequests/UserEfficiency.jsx
@@ -3,80 +3,91 @@ import React, { useState, useEffect } from "react";
 import "./UserRequests.css";
 import Dropdown from "react-bootstrap/Dropdown";
 import axios from "axios";
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import DropdownButton from "react-bootstrap/DropdownButton";
 import { Button } from "react-bootstrap";
 
 function UserEfficiency() {
-
-  const [data, setData] = useState([]);
-
-//   function GetBadgeGroups() {
-//     axios.get("http://localhost:8000/badges/")
-//     .then(response => {
-//       setData(response.data);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-//   }
-// };
-
-
+  const [badgesGroups, setBadgesGroups] = useState([]);
+  const [badges, setBadges] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:8000/badges/")
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
+    axios
+      .all([
+        axios.get("http://localhost:8000/badges/"),
+        axios.get("http://localhost:8000/level_reports/"),
+      ])
+      .then(
+        axios.spread((badgesGroupsResponse, badgesResponse) => {
+          setBadgesGroups(badgesGroupsResponse.data);
+          setBadges(badgesResponse.data);
+        })
+      )
+      .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  const [value, setValue]=useState('');
+  const [choosenGroup, setChoosenGroup] = useState("");
+  const [choosenBadge, setChoosenBadge] = useState("");
 
-  const handleSelect=(e)=>{
-    setValue(e);
-    console.log(e);
+  const handleSelect = (group) => {
+    setChoosenGroup(group);
+    console.log(group);
+  };
+  const handleSelect1 = (badg) => {
+    setChoosenBadge(badg);
+    console.log(badg);
+  };
+
+  function PostBadge() {
+    axios
+      .post("http://localhost:8000/badge_reports/", {
+        title: choosenGroup,
+        status: "zgłoszona",
+        user_id: 1,
+        badge_id: 3,
+      })
+      .then(function (response) {
+        console.log(response);
+      });
   }
 
- function PostBadge() {
-  axios.post("http://localhost:8000/badge_reports/",{
-  // axios.post("http://localhost:8000/badge_reports/groups/",{
-    "title": value,
-    "status": "zgłoszona",
-    "user_id": 1,
-    "badge_id": 3,
-  })
-  .then(function(response){
-    console.log(response);
-  })
-  };
- 
-
-
-    return(
-        <div className="jumbotron UserEfficiencyStyle rounded" >
-          <h1>Zakładka służąca rozpoczęcia nowej sprawnośći</h1>
-          <DropdownButton
+  return (
+    <div className="jumbotron UserEfficiencyStyle rounded">
+      <h1>Zakładka służąca rozpoczęcia nowej sprawnośći</h1>
+      <DropdownButton
         alignRight
         title="Wybierz sprawność"
         id="dropdown-menu-align-right"
-        // onSelect={handleSelect}>
-        //         {data.map(report => (
-        //   <Dropdown.Item key={report.id} eventKey={report.name} onSelect={handleSelect}>{report.name}</Dropdown.Item>
-        // ))}
-        // </DropdownButton>
-        onSelect={handleSelect}>
-                {data.map(report => (
-          <Dropdown.Item key={report.id} eventKey={report.name} onSelect={handleSelect}>{report.name}</Dropdown.Item>
+        onSelect={handleSelect}
+      >
+        {badgesGroups.map((report) => (
+          <Dropdown.Item key={report.id} eventKey={report.name}>
+            {report.name}
+          </Dropdown.Item>
         ))}
-        </DropdownButton>
-    <h1>Wybrałeś wartość {value}</h1>
-    <Button onClick={() => PostBadge()}>Rozpocznij sprawność</Button>
+      </DropdownButton>
+      <DropdownButton
+        alignRight
+        title="Wybierz sprawność"
+        id="dropdown-menu-align-right"
+        onSelect={handleSelect1}
+      >
+        {badges.map((badge) => (
+          <Dropdown.Item
+            key={badge.id}
+            eventKey={badge.title}
+            // onClick={badgeSelect}
+          >
+            {badge.title}
+          </Dropdown.Item>
+        ))}
+      </DropdownButton>
+      <h4>Grupa sprawności: {choosenGroup}</h4>
+      <h4>Sprawność: {choosenBadge}</h4>
+      <Button onClick={() => PostBadge()}>Rozpocznij sprawność</Button>
     </div>
-    );
-  }
-
+  );
+}
 
 export default UserEfficiency;
