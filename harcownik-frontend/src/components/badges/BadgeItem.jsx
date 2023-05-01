28694aeec144e95309913b3f265bdeef2694fe24
pawel150199 <pawel.polski99@gmail.com>
2023-05-01 17:31:31 +0200

    try to fix badges

--- harcownik-frontend/src/components/badges/BadgeItem.jsx
+++ harcownik-frontend/src/components/badges/BadgeItem.jsx
@@ -1,24 +1,16 @@
-/* eslint-disable */
 import React from "react";
 import { Card } from "react-bootstrap";
 
 const BadgeItem = ({ badge }) => {
   return (
-    <Card>
-      <div style={{ position: "relative" }}>
-        <div style={{
-          position: "absolute",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-          padding: "0.5rem",
-        }}>
+    <Card key={badge.id} style={{ marginBottom: "1rem" }}>
+      <div id="item-1">
+        <div id="item-2">
           <Card.Title style={{ color: "white" }}>{badge.name}</Card.Title>
         </div>
       </div>
-      <Card.Body>
-          <Card.Text key={badge.index} style={{ margin: "0.5rem 0" }}> {badge.description} </Card.Text>
+      <Card.Body className="text-center">
+        <Card.Text style={{ margin: "0.5rem 0" }}>{badge.description}</Card.Text>
       </Card.Body>
     </Card>
   );
--- harcownik-frontend/src/components/badges/Badges.jsx
+++ harcownik-frontend/src/components/badges/Badges.jsx
@@ -1,93 +1,52 @@
 import axios from "axios";
-import React from "react";
-import { useState, useEffect } from "react";
-import { Accordion } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Accordion, Container, Spinner } from "react-bootstrap";
 
 import BadgeItem from "./BadgeItem";
-
+import "./badges.css";
 
 const Badges = () => {
-
   const [badges, setBadges] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    axios.get("http://localhost:8000/badges/grouped")
-      .then(response => {
+    axios
+      .get("http://localhost:8000/badges/grouped")
+      .then((response) => {
         setBadges(response.data);
+        setIsLoading(false);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error fetching data from API: " + error);
-      })
+        setIsLoading(false);
+      });
   }, []);
 
-  console.log("XDDD" + badges)
-
-/*
-  const badges = [
-    {
-      group: "Sprawności Łącznościowe",
-      badges: [
-        {
-          name: "Goniec",
-          description: [
-            "Wskazała / wskazał drogę do urzędów i ważnych miejsc w swojej miejscowości",
-            "Dostarczyła / dostarczył list lub ustną wiadomość do adresata w wyznaczonym terminie",
-            "Nadała / nadał na poczcie: list polecony, przekaz pieniężny, paczkę",
-            "Bezbłędnie przekazała / przekazał meldunek w czasie gry terenowej, posługując się wybranymi sposobami łączności"
-          ],
-        },
-        {
-          name: "Zwiadowca",
-          description: [
-            "W nieznanym terenie niedostrzeżenie podeszła / podszedł wskazany obiekt",
-            "Przeprowadziła / przeprowadził zwiad, sporządzając szczegółowy raport z opisem wykonanych zadań i zaobserwowanych zdarzeń",
-            "Nawiązała / nawiązał łączność za pomocą radia CB, przekazała / przekazał informacje zdobyte podczas zwiadu",
-            "Wie, jakie są zasady i sposoby przekazywania oraz gromadzenia poufnych informacji (np. danych osobowych), szczególnie w Internecie"
-          ],
-        },
-        // add more badges here
-      ],
-    },
-    {
-      group: "Sprawności Inne",
-      badges: [
-        {
-          name: "Goniec",
-          description: [
-            "Wskazała / wskazał drogę do urzędów i ważnych miejsc w swojej miejscowości",
-            "Dostarczyła / dostarczył list lub ustną wiadomość do adresata w wyznaczonym terminie",
-            "Nadała / nadał na poczcie: list polecony, przekaz pieniężny, paczkę",
-            "Bezbłędnie przekazała / przekazał meldunek w czasie gry terenowej, posługując się wybranymi sposobami łączności"
-          ],
-        },
-        {
-          name: "Zwiadowca",
-          description: [
-            "W nieznanym terenie niedostrzeżenie podeszła / podszedł wskazany obiekt",
-            "Przeprowadziła / przeprowadził zwiad, sporządzając szczegółowy raport z opisem wykonanych zadań i zaobserwowanych zdarzeń",
-            "Nawiązała / nawiązał łączność za pomocą radia CB, przekazała / przekazał informacje zdobyte podczas zwiadu",
-          ],
-        },
-        // add more badges here
-      ]
-    },
-    // add more groups here
-  ];
-*/
+  if (isLoading) {
+    return (
+      <Container className="text-center">
+        <Spinner animation="border" variant="primary" />
+        <div>Loading badges...</div>
+      </Container>
+    );
+  }
 
   return (
-    <Accordion>
-      {badges.map((group) => (
-        <Accordion.Item key={group.id} eventKey={group.id} className="mb-3">
-          <Accordion.Header>{group.group}</Accordion.Header>
+    <Container id="container">
+      <h2 id="badges">Sprawności harcerzy polskich</h2>
+      <Accordion>
+        {badges.map((group, index) => (
+          <Accordion.Item key={index} eventKey={index} className="mb-3">
+          <Accordion.Header className="text-center">{group.group}</Accordion.Header>
           <Accordion.Body>
-            {group.badges.map((badge) => (
-              <BadgeItem badge={badge} />
+            {group.badges.map((badge, index) => (
+              <BadgeItem key={index} badge={badge} />
             ))}
           </Accordion.Body>
         </Accordion.Item>
-      ))}
-    </Accordion>
+        ))}
+      </Accordion>
+    </Container>
   );
 };
 
