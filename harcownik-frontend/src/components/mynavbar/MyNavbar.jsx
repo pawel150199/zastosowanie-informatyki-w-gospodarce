21ef25368d4d0ca628bbb3b112690861a50bd206
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-05-03 22:21:42 +0200

    fix url and badges (#34)
    * fix url and badges
    
    * fix url and badges

--- harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
+++ harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
@@ -1,35 +1,51 @@
-import { Navbar, Nav, Container, Image, NavDropdown} from "react-bootstrap";
+import { Navbar, Nav, Container, Image, NavDropdown } from "react-bootstrap";
 import React from "react";
-import  "./mynavbar.css";
+import "./mynavbar.css";
 
 const MyNavbar = () => {
-
-    return (
-        <Navbar  expand="lg" className="navbar-nav navbar-dark bg-dark h1 fs-4"> 
-            <Container>
-                <Navbar.Brand href="/" className="navbar-nav mr-left">
-                        <Image src="/img/logo.png" id="img" style={{width: "1.8em"}}/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-left" />
-                <Navbar.Collapse id="basic-navbar-nav" >
-                    <Nav className="navbar-nav mx-auto" >
-                        <Nav.Link className="box" href="/" >Strona Główna</Nav.Link>
-                        <Nav.Link className="box" href="/login"> Logowanie</Nav.Link>
-                        <Nav.Link className="box" href="/about"> O nas</Nav.Link>
-                        <Nav.Link className="box" href="/badges"> Sprawności</Nav.Link>
-                        <Nav.Link className="box" href="/raport"> Raport</Nav.Link>
-                        <Nav.Link className="box" href="/userRequests"> Zgłoszenia użytkownika</Nav.Link>
-                    </Nav>
-                    <Nav className="navbar-nav navbar-dark mr-auto">
-                        <NavDropdown title="Menu" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/user">Profil</NavDropdown.Item>
-                            <NavDropdown.Item href="#">Wyloguj</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar expand="lg" className="navbar-nav navbar-dark bg-dark h1 fs-4">
+      <Container>
+        <Navbar.Brand href="/" className="navbar-nav mr-left">
+          <Image src="/img/logo.png" id="img" style={{ width: "1.8em" }} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-left" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="navbar-nav mx-auto">
+            <Nav.Link className="box" href="/">
+              Strona Główna
+            </Nav.Link>
+            <Nav.Link className="box" href="/login">
+              {" "}
+              Logowanie
+            </Nav.Link>
+            <Nav.Link className="box" href="/about">
+              {" "}
+              O nas
+            </Nav.Link>
+            <Nav.Link className="box" href="/badges">
+              {" "}
+              Sprawności
+            </Nav.Link>
+            <Nav.Link className="box" href="/raport">
+              {" "}
+              Raport
+            </Nav.Link>
+            <Nav.Link className="box" href="/user_Requests">
+              {" "}
+              Zgłoszenia użytkownika
+            </Nav.Link>
+          </Nav>
+          <Nav className="navbar-nav navbar-dark mr-auto">
+            <NavDropdown title="Menu" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/user">Profil</NavDropdown.Item>
+              <NavDropdown.Item href="#">Wyloguj</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default MyNavbar;
