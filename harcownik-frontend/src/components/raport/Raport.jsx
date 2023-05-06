85ea1f1d98f91b14d4206674c32978f96f909673
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-05-06 15:14:36 +0200

    valuetext

--- harcownik-frontend/src/components/raport/Raport.jsx
+++ harcownik-frontend/src/components/raport/Raport.jsx
@@ -1,31 +1,40 @@
+/* eslint-disable */
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
+import "./raport_style.css";
 
 import RaportHeadear from "./RaportHeadear";
 import Submissions from "./Submissions";
 import SelectionOfTabs from "./SelectionOfTabs";
 import Generate from "./Generate";
 import Efficiency from "./Efficiency";
-
-import "./raport_style.css";
+import { tabs } from "./SelectionOfTabs";
 
 const Raport = () => {
-
   return (
-    <div  className="container">
+    <div className="container">
       <Row>
         <RaportHeadear />
       </Row>
       <Row>
-        <Col md={7}>
+        <Col md={6}>
           <Submissions />
+        </Col>
+        <Col md={6}>
           <Efficiency />
         </Col>
-        <Col md={5}>
-          <SelectionOfTabs />
-          <Generate />
-        </Col>
       </Row>
+      <Row>
+        {/* <Col md={6}> */}
+        <SelectionOfTabs />
+        {/* </Col> */}
+        {/* <Col md={6}>
+          <Generate />
+        </Col> */}
+      </Row>
+      {/* <Row>
+        <GenerateTextArea />
+      </Row> */}
     </div>
   );
 };
