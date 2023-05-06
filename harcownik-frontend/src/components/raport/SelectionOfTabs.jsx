793fab2647079f60feabc614c76bd81e12fa0122
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-05-06 15:14:55 +0200

    valuetext

--- harcownik-frontend/src/components/raport/SelectionOfTabs.jsx
+++ harcownik-frontend/src/components/raport/SelectionOfTabs.jsx
@@ -1,22 +1,96 @@
+/* eslint-disable */
 import React, { useState } from "react";
-import {
-  Form,
-  Dropdown,
-} from "react-bootstrap";
+import { Form, Dropdown, FormGroup, FormControl } from "react-bootstrap";
 import "./raport_style.css";
 
+const [tabs, setTabs] = useState([
+  {
+    id: "item1",
+    label: "Wstęp",
+    isChecked: false,
+    patternText:
+      "Druhny i Druhowie!Minęło już 50 lat od powstania naszej drużyny. Każda harcerka i harcerz powinni poszukiwać w starszcyh pokoleniach wzorów swoich postaw, które będą stanowiły drogowskazy podejmowanych wyzwań i dokonywania wyborów życiowych....",
+    textValue: "",
+  },
+  {
+    id: "item2",
+    label: "Wyjątki z rozkazu",
+    isChecked: false,
+    patternText:
+      "Druhny i Druhowie!Minęło już 50 lat od powstania naszej drużyny. Każda harcerka i harcerz powinni poszukiwać w starszcyh pokoleniach wzorów swoich postaw, które będą stanowiły drogowskazy podejmowanych wyzwań i dokonywania wyborów życiowych....",
+    textValue: "",
+  },
+  {
+    id: "item3",
+    label: "Uchwały",
+    isChecked: false,
+    patternText:
+      "Druhny i Druhowie!Minęło już 50 lat od powstania naszej drużyny. Każda harcerka i harcerz powinni poszukiwać w starszcyh pokoleniach wzorów swoich postaw, które będą stanowiły drogowskazy podejmowanych wyzwań i dokonywania wyborów życiowych....",
+    textValue: "",
+  },
+  {
+    id: "item4",
+    label: "Zwolnienia",
+    isChecked: false,
+    patternText:
+      "Druhny i Druhowie!Minęło już 50 lat od powstania naszej drużyny. Każda harcerka i harcerz powinni poszukiwać w starszcyh pokoleniach wzorów swoich postaw, które będą stanowiły drogowskazy podejmowanych wyzwań i dokonywania wyborów życiowych....",
+    textValue: "",
+  },
+  {
+    id: "item5",
+    label: "Przyznanie stopni,sprawności",
+    isChecked: false,
+    patternText:
+      "Druhny i Druhowie!Minęło już 50 lat od powstania naszej drużyny. Każda harcerka i harcerz powinni poszukiwać w starszcyh pokoleniach wzorów swoich postaw, które będą stanowiły drogowskazy podejmowanych wyzwań i dokonywania wyborów życiowych....",
+    textValue: "",
+  },
+]);
 
 function SelectionOfTabs() {
-  // const [pdf, setPdf] = useState(null);
-  // const [pageNumber, setPageNumber] = useState(1);
-  // const [editing, setEditing] = useState(false);
-  // const [canvas, setCanvas] = useState(null);
-
-  // const handleEditClick = () => {
-  //   setEditing(true);
-  // };
-
   const [selectedItems, setSelectedItems] = useState([]);
+
+  // const [tabs, setTabs] = useState([
+  //   {
+  //     id: "item1",
+  //     label: "Wstęp",
+  //     isChecked: false,
+  //     patternText:
+  //       "Druhny i Druhowie!Minęło już 50 lat od powstania naszej drużyny. Każda harcerka i harcerz powinni poszukiwać w starszcyh pokoleniach wzorów swoich postaw, które będą stanowiły drogowskazy podejmowanych wyzwań i dokonywania wyborów życiowych....",
+  //     textValue: "",
+  //   },
+  //   {
+  //     id: "item2",
+  //     label: "Wyjątki z rozkazu",
+  //     isChecked: false,
+  //     patternText:
+  //       "Druhny i Druhowie!Minęło już 50 lat od powstania naszej drużyny. Każda harcerka i harcerz powinni poszukiwać w starszcyh pokoleniach wzorów swoich postaw, które będą stanowiły drogowskazy podejmowanych wyzwań i dokonywania wyborów życiowych....",
+  //     textValue: "",
+  //   },
+  //   {
+  //     id: "item3",
+  //     label: "Uchwały",
+  //     isChecked: false,
+  //     patternText:
+  //       "Druhny i Druhowie!Minęło już 50 lat od powstania naszej drużyny. Każda harcerka i harcerz powinni poszukiwać w starszcyh pokoleniach wzorów swoich postaw, które będą stanowiły drogowskazy podejmowanych wyzwań i dokonywania wyborów życiowych....",
+  //     textValue: "",
+  //   },
+  //   {
+  //     id: "item4",
+  //     label: "Zwolnienia",
+  //     isChecked: false,
+  //     patternText:
+  //       "Druhny i Druhowie!Minęło już 50 lat od powstania naszej drużyny. Każda harcerka i harcerz powinni poszukiwać w starszcyh pokoleniach wzorów swoich postaw, które będą stanowiły drogowskazy podejmowanych wyzwań i dokonywania wyborów życiowych....",
+  //     textValue: "",
+  //   },
+  //   {
+  //     id: "item5",
+  //     label: "Przyznanie stopni,sprawności",
+  //     isChecked: false,
+  //     patternText:
+  //       "Druhny i Druhowie!Minęło już 50 lat od powstania naszej drużyny. Każda harcerka i harcerz powinni poszukiwać w starszcyh pokoleniach wzorów swoich postaw, które będą stanowiły drogowskazy podejmowanych wyzwań i dokonywania wyborów życiowych....",
+  //     textValue: "",
+  //   },
+  // ]);
 
   const handleSelect = (eventKey) => {
     if (!selectedItems.includes(eventKey)) {
@@ -27,93 +101,70 @@ function SelectionOfTabs() {
   const handleDeselect = (eventKey) => {
     setSelectedItems(selectedItems.filter((item) => item !== eventKey));
   };
- 
-    return(
-        <div className="jumbotron  jumbotronStyle_2 rounded">
-        <h1>Podpunkty do rozkazu</h1>
-        <Dropdown onSelect={handleSelect} onDeselect={handleDeselect}>
-          <Dropdown.Toggle variant="primary" id="multi-checkbox-dropdown" style={{ width: "100%" ,backgroundColor: "#e0a158"}}>
-            {selectedItems.length === 0
-              ? "Wybierz kategorie"
-              : `${selectedItems.length} kategorie`}
-          </Dropdown.Toggle>
-          <Dropdown.Menu style={{ width: "100%", backgroundColor: "#d6c0a8" }}>
-            <Form>
-              <Form.Check
-                type="checkbox"
-                label="Wstęp"
-                id="item1"
-                checked={selectedItems.includes("item1")}
-                onChange={(e) => {
-                  if (e.target.checked) {
-                    handleSelect("item1");
-                  } else {
-                    handleDeselect("item1");
-                  }
-                }}
-              />
-              
-              <Form.Check
-                type="checkbox"
-                label="Wyjątki z rozkazu"
-                id="item2"
-                checked={selectedItems.includes("item2")}
-                onChange={(e) => {
-                  if (e.target.checked) {
-                    handleSelect("item2");
-                  } else {
-                    handleDeselect("item2");
-                  }
-                }}
-              />
-              
-              <Form.Check
-                type="checkbox"
-                label="Uchwały"
-                id="item3"
-                checked={selectedItems.includes("item3")}
-                onChange={(e) => {
-                  if (e.target.checked) {
-                    handleSelect("item3");
-                  } else {
-                    handleDeselect("item3");
-                  }
-                }}
-              />
-              
-              <Form.Check
-                type="checkbox"
-                label="Zwolnienia"
-                id="item4"
-                checked={selectedItems.includes("item4")}
-                onChange={(e) => {
-                  if (e.target.checked) {
-                    handleSelect("item4");
-                  } else {
-                    handleDeselect("item4");
-                  }
-                }}
-              />
-              
-              <Form.Check
-                type="checkbox"
-                label="Przyznanie stopni,sprawności"
-                id="item5"
-                checked={selectedItems.includes("item5")}
-                onChange={(e) => {
-                  if (e.target.checked) {
-                    handleSelect("item5");
-                  } else {
-                    handleDeselect("item5");
-                  }
-                }}
-              />
-              
-            </Form>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
-    );
+
+  const handleTextValueChange = (event, tab) => {
+    const updatedTabs = tabs.map((t) => {
+      if (t.id === tab.id) {
+        return { ...t, textValue: event.target.value };
+      } else {
+        return t;
+      }
+    });
+    setTabs(updatedTabs);
+    // console.log("Updated tab:", tab);
+  };
+
+  return (
+    <div className="jumbotron jumbotronStyle_2 rounded">
+      <h1>Podpunkty do rozkazu</h1>
+      <Form>
+        {tabs.map((tab) => (
+          <div key={tab.id}>
+            <Form.Check
+              type="checkbox"
+              label={tab.label}
+              id={tab.id}
+              checked={selectedItems.includes(tab.id)}
+              onChange={(e) => {
+                if (e.target.checked) {
+                  handleSelect(tab.id);
+                  tab.isChecked = true;
+                  console.log("ischecked", tab.isChecked, tab.label);
+                } else {
+                  handleDeselect(tab.id);
+                  console.log("ischecked", tab.isChecked, tab.label);
+                }
+              }}
+            />
+            {selectedItems.includes(tab.id) && (
+              // <div className="form-group">
+              //   {/* <label htmlFor="exampleInput">{tab.label}</label> */}
+              //   <input
+              //     type="text"
+              //     className="form-control"
+              //     id={tab.id}
+              //     defaultValue={tab.patternText}
+              //     onChange={(e) => handleTextValueChange(e, tab)}
+              //   />
+              //   <h1>{tab.textValue}</h1>
+              // </div>
+              <Form.Group controlId="exampleForm.ControlTextarea1">
+                {/* <Form.Label>Przykładowy tekst</Form.Label> */}
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  value={tab.value}
+                  defaultValue={tab.patternText}
+                  onChange={(e) => handleTextValueChange(e, tab)}
+                />
+                <h1>{tab.textValue}</h1>
+              </Form.Group>
+            )}
+          </div>
+        ))}
+      </Form>
+    </div>
+  );
 }
 
 export default SelectionOfTabs;
