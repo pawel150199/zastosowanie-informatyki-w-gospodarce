74609c5459672a4f3c2ce3835531c8d993f3f6d6
pawel150199 <pawel.polski99@gmail.com>
2023-05-07 12:17:59 +0200

    add user_request to routing

--- harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
+++ harcownik-frontend/src/components/mynavbar/MyNavbar.jsx
@@ -34,7 +34,7 @@ const MyNavbar = () => {
                         <Nav.Link className="box" href="/about"> O nas</Nav.Link>
                         <Nav.Link className="box" href="/badges"> Sprawności</Nav.Link>
                         <Nav.Link className="box" href="/raport"> Raport</Nav.Link>
-                        <Nav.Link className="box" href="/user_requests"></Nav.Link>
+                        <Nav.Link className="box" href="/user_requests">Zgłoszenia użytkownika</Nav.Link>
                     </Nav>
                     <Nav className="navbar-nav navbar-dark mr-auto">
                     {getLoginStatus("isLogged") ? (
