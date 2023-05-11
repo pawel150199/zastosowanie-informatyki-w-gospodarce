16288ec40c780ea84fa7591c679f8f8302146890
Michal170 <93790482+Michal170@users.noreply.github.com>
2023-05-11 22:30:14 +0200

    Use /me endpoint in layout_2

--- harcownik-frontend/src/components/raport/RaportFunction.jsx
+++ harcownik-frontend/src/components/raport/RaportFunction.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const getLevelApplications = async () => {
   try {
-    const response = await axios.get("http://localhost:8000/level_reports/z");
+    const response = await axios.get("http://localhost:8000/level_reports/");
     console.log("GetLevelApplications: ", response);
     return response.data;
   } catch (error) {
