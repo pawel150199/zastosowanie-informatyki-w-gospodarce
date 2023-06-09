94fe3416669984c920a19c8c596445fcb8fc605b
pawel150199 <pawel.polski99@gmail.com>
2023-06-09 16:37:23 +0200

    fix backend

--- harcownik-backend/src/db/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.db.db import Base
-from src.models.badge import Badge
-from src.models.user import User
-from src.models.badge_report import BadgeReport
-from src.models.level_report import LevelReport
-from src.models.group import Group
