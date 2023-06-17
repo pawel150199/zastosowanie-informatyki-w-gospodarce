421a5240f0ef3723f8eebcd2027c5bee30b6d356
pawel150199 <pawel.polski99@gmail.com>
2023-06-17 17:27:30 +0200

    add endpoint

--- harcownik-backend/src/api/endpoints/level_reports.py
+++ harcownik-backend/src/api/endpoints/level_reports.py
@@ -49,15 +49,15 @@ def read_level_reports_in_my_group(
 #        )
 #    return level_reports
 
-# @router.get("/level_reports/{level_report_id}", response_model=schemas.LevelReport)
-# def read_level_report(level_report_id: int, db: Session = Depends(get_db)):
-#    db_level_report = crud.get_level_report(db=db, level_report_id=level_report_id)
-#    if db_level_report is None:
-#        raise HTTPException(
-#            status_code=404,
-#            detail="Report not found"
-#        )
-#    return db_level_report
+@router.get("/level_reports/{level_report_id}", response_model=schemas.LevelReport)
+def read_level_report(level_report_id: int, db: Session = Depends(get_db), _: models.User = Depends(get_current_teamadmin)):
+    db_level_report = crud.get_level_report(db=db, level_report_id=level_report_id)
+    if db_level_report is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Report not found"
+        )
+    return db_level_report
 
 
 @router.get("/me/level_reports", response_model=list[schemas.LevelReport])
